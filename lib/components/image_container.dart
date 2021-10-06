ec5764589edf47e23d0b148321acd5f80428caaf
Luiz Tucillo <luiz.tucillo@mercadolivre.com>
2021-10-06 16:26:33 -0300

    Improve interface

--- lib/components/image_container.dart
+++ lib/components/image_container.dart
@@ -8,7 +8,7 @@ import '../models/image_model.dart';
 class ImageContainer extends StatelessWidget {
   final ImageModel image;
   final bool isSelected;
-  final  VoidCallback onDelete;
+  final VoidCallback onDelete;
 
   const ImageContainer({
     Key? key,
@@ -22,144 +22,141 @@ class ImageContainer extends StatelessWidget {
     return Consumer<SelectionModeProvider>(
       builder: (context, selectionModeProvider, widget) => Stack(
         children: [
-          Container(
-            decoration: ShapeDecoration(
-              color: isSelected ? Colors.blue.withAlpha(50) : Colors.grey[100],
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(6),
+          Opacity(
+            opacity: image.converted ? 1 : 0.2,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: FileImage(image.file),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
-          Opacity(
-            opacity: image.converted ? 1 : 0.2,
-            child: TextButton(
-              child: Center(
-                child: Image.file(image.file),
-              ),
-              onLongPress: () {
-                if (!image.converted) {
-                  return;
-                }
+          TextButton(
+            child: Container(),
+            onLongPress: () {
+              if (!image.converted) {
+                return;
+              }
 
+              selectionModeProvider.toggleSelection(image);
+            },
+            onPressed: () {
+              if (!image.converted) {
+                return;
+              }
+
+              if (selectionModeProvider.inSelectionMode) {
                 selectionModeProvider.toggleSelection(image);
-              },
-              onPressed: () {
-                if (!image.converted) {
-                  return;
-                }
+                return;
+              }
 
-                if (selectionModeProvider.inSelectionMode) {
-                  selectionModeProvider.toggleSelection(image);
-                  return;
-                }
-
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Material(
-                        color: Colors.transparent,
-                        child: GestureDetector(
-                          onTap: () {},
-                          child: Center(
-                            child: Stack(
-                              alignment: Alignment.bottomRight,
-                              children: [
-                                Image.file(
-                                  image.file,
-                                ),
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    TextButton(
-                                      child: Container(
-                                        padding: const EdgeInsets.all(8),
-                                        decoration: const BoxDecoration(
-                                          color: Colors.white,
-                                          shape: BoxShape.circle,
-                                        ),
-                                        child: const Icon(Icons.delete),
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Material(
+                      color: Colors.transparent,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Center(
+                          child: Stack(
+                            alignment: Alignment.bottomRight,
+                            children: [
+                              Image.file(
+                                image.file,
+                              ),
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  TextButton(
+                                    child: Container(
+                                      padding: const EdgeInsets.all(8),
+                                      decoration: const BoxDecoration(
+                                        color: Colors.white,
+                                        shape: BoxShape.circle,
                                       ),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                        onDelete();
-                                      },
+                                      child: const Icon(Icons.delete),
                                     ),
-                                    TextButton(
-                                      child: Container(
-                                        padding: const EdgeInsets.all(8),
-                                        decoration: const BoxDecoration(
-                                          color: Colors.white,
-                                          shape: BoxShape.circle,
-                                        ),
-                                        child: const Icon(Icons.share),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                      onDelete();
+                                    },
+                                  ),
+                                  TextButton(
+                                    child: Container(
+                                      padding: const EdgeInsets.all(8),
+                                      decoration: const BoxDecoration(
+                                        color: Colors.white,
+                                        shape: BoxShape.circle,
                                       ),
-                                      onPressed: () {
-                                        Share.shareFiles([image.file.path]);
-                                      },
+                                      child: const Icon(Icons.share),
                                     ),
-                                    TextButton(
-                                      child: Container(
-                                        padding: const EdgeInsets.all(8),
-                                        decoration: const BoxDecoration(
-                                          color: Colors.white,
-                                          shape: BoxShape.circle,
-                                        ),
-                                        child: const Icon(Icons.close),
+                                    onPressed: () {
+                                      Share.shareFiles([image.file.path]);
+                                    },
+                                  ),
+                                  TextButton(
+                                    child: Container(
+                                      padding: const EdgeInsets.all(8),
+                                      decoration: const BoxDecoration(
+                                        color: Colors.white,
+                                        shape: BoxShape.circle,
                                       ),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
+                                      child: const Icon(Icons.close),
                                     ),
-                                  ],
-                                ),
-                              ],
-                            ),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                    );
-                  },
-                );
-              },
-            ),
+                    ),
+                  );
+                },
+              );
+            },
           ),
           image.converted
               ? const IgnorePointer()
               : const IgnorePointer(
                   child: Center(child: CircularProgressIndicator()),
                 ),
-          IgnorePointer(
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              alignment: Alignment.bottomRight,
-              child: !isSelected
-                  ? Container()
-                  : const Icon(
-                      Icons.check_box,
+          !isSelected
+              ? Container()
+              : IgnorePointer(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blueAccent, width: 3),
+                    ),
+                    alignment: Alignment.bottomRight,
+                    child: const Icon(
+                      Icons.check,
                       size: 30,
                       color: Colors.blue,
                     ),
-            ),
-          ),
+                  ),
+                ),
           IgnorePointer(
             child: Container(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
               alignment: Alignment.bottomLeft,
               child: Container(
                 padding: const EdgeInsets.all(5),
                 child: Text(
                   image.imageType.extension().toUpperCase(),
-                  style: const TextStyle(fontSize: 12),
+                  style: const TextStyle(fontSize: 12, color: Colors.white),
                 ),
-                decoration: BoxDecoration(
-                  color: Colors.white.withAlpha(200),
-                  borderRadius: const BorderRadius.all(Radius.circular(5)),
-                ),
+                color: Colors.black.withAlpha(150),
               ),
             ),
           ),
